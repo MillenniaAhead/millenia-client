5ecd4453945d7d73c32656605e3a39ed851511c1
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-11-11 20:21:21 +0600

    team size almost done

--- src/Components/TeamSize.js
+++ src/Components/TeamSize.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CSS/Style.css'
+import idea from '../images/team/Idea.jpg';
 
 const TeamSize = () => {
     return (
@@ -10,17 +11,31 @@ const TeamSize = () => {
                     <button type="button" class="btn btn-link linkButton">Previous</button></div>
                   <div><button type="button" class="btn btn-secondary nextButton">Next Step</button></div>
             </div>
-            <div>
+            <hr
+        style={{
+          background: 'gray',height: '3px',
+        }}
+      />
+            <div >
                 <p >Business setup</p>
                 <h3 className='text-center'>What’s your team size?</h3>
                 <p>This will help us set up your calender correctly. Don’t worry, this doesn’t change the price- you can have unlimited team <br />
 members for free on salon!</p>
                    <div className='teamMember'>
-            <input className='p-3 mt-5 mx-4' type="radio" name="me" id="" value="" />It’s just me <br />
-            <input className='p-3  mt-5 mx-4' type="radio" name="me" id="" value="" />2-5 people
+         
+           <input className='mt-5 mx-4' type="radio" name="me" id="" value="" />
+            <label htmlFor="team1">It’s just me</label> 
             <br />
-            <input className='p-3  mt-5 mx-4' type="radio" name="me" id="" value="" />6-10 people <br />
-            <input className='p-3  mt-5 mx-4' type="radio" name="me" id="" value="" />11+ people
+            <input className=' mt-5 mx-4' type="radio" name="me" id="" value="" /><label htmlFor="">2-5 people</label>
+            <br />
+            <input className='mt-5 mx-4' type="radio" name="me" id="" value="" /><label htmlFor="">6-10 people </label><br />
+            <input className=' mt-5  mx-4' type="radio" name="me" id="" value="" /><label htmlFor="">11+ people</label>
+
+            <div style={{margin:'22px', backgroundColor:'#DAE9F7', borderRadius:'6px'}}className='d-flex justify-content-center'>
+                <div className='me-2'><img src={idea} alt="" /></div>
+               <div> <p>We’ll add  ‘Wendy’ as an example employee so you can see how the <br />
+ system works. You can manage employees later once you’re in!</p></div>
+            </div>
                    </div>
             </div>
         </section>
