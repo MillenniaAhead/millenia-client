a636bcab35288f1291b66db914b219547e69d242
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-11-11 22:24:26 +0600

    business setUp

--- src/Components/BusinessSetup.js
+++ src/Components/BusinessSetup.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './CSS/Style.css'
-
+import Accordion from 'react-bootstrap/Accordion';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
+import { GrLocation } from 'react-icons/gr';
 const BusinessSetup = () => {
     return (
         <section>
@@ -19,7 +22,31 @@ const BusinessSetup = () => {
                 <p >Business setup</p>
                 <h3 className='text-center'>Set your location</h3>
                 <p>Add your business location so your clients can easily find you.</p>
-                   
+                <div   style={{width:'835px', margin:'auto'}}>
+                <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Business location</Accordion.Header>
+        <Accordion.Body>
+        <p className='location'>Where’s your business located?</p>
+        <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">
+        <GrLocation/>
+        </InputGroup.Text>
+       
+        <Form.Control
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+      <div className='d-flex'>
+      <input type="checkbox"className='m-1' name="" id="" /> <p>I don’t have a business address ( mobile and online services only)</p>
+      </div>
+        </Accordion.Body>
+        
+      </Accordion.Item>
+    </Accordion>
+                </div>
             </div>
         </section>
     );
