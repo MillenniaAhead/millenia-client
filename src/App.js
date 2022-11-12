335eb9e81c67e14d8180fe05663c3b64ebabcd5e
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-11-12 12:21:09 +0600

    hear about source

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import BusinessSetup from './Components/BusinessSetup';
+import HearAbout from './Components/HearAbout';
 import Software from './Components/Software';
 import TeamSize from './Components/TeamSize';
 
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<TeamSize/>}> </Route>
         <Route path='/setUp' element={<BusinessSetup></BusinessSetup>}/>
         <Route path='/software' element={<Software></Software>}></Route>
+        <Route path='/hear' element={<HearAbout></HearAbout>}></Route>
       </Routes>
     </div>
   );
--- src/Components/HearAbout.js
+++ src/Components/HearAbout.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import Upperbutton from './Upperbutton';
 
 const HearAbout = () => {
     return (
         <section>
+        <Upperbutton></Upperbutton>
+         <div >
+             <p >Business setup</p>
+             <h3 className='text-center'>How did you hear about OUTLETCONTROL ?</h3>
+             
+                <div className='teamMember'>
+      
+        <input className='mt-5 mx-4' type="radio" name="" id="" value="" />
+         <label htmlFor="team1">Recommended by a friend</label> 
+         <br />
+         <input className=' mt-5 mx-4' type="radio" name="" id="" value="" /><label htmlFor="">Search engine (e.g. Google, Yahoo)</label>
+         <br />
+         <input className='mt-5 mx-4' type="radio" name="" id="" value="" /><label htmlFor="">Social media</label>
+         <br />
+         <input className=' mt-5  mx-4' type="radio" name="" id="" value="" /><label htmlFor="">Magazine ad</label>
+         <br />
+         <input className=' mt-5  mx-4' type="radio" name="" id="" value="" /><label htmlFor="">Ratings website (e.g. Capterra, Trustpilot)</label>
+         <br />
+         <input className=' my-5  mx-4' type="radio" name="" id="" value="" /><label htmlFor="">Other</label>
+         
 
-            
-        </section>
+         
+                </div>
+         </div>
+     </section>
     );
 };
 
